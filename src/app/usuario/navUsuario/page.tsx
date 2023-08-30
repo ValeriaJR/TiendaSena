c89507fcc5746d8958612d121982aa9c94c283fb
Valeria <jvaleria826@gmail.com>
2023-08-30 14:27:41 -0500

    initial 2

---
+++ src/app/usuario/navUsuario/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+export default function NavUsuario(){
+    return(
+        <>
+        <div className="row">
+            <div className="col-1"><image></image></div>
+            <div className="col-4">La Tienda del Cafe</div>
+            <div className="col-5"></div>
+        </div>
+        </>
+    )
+}
